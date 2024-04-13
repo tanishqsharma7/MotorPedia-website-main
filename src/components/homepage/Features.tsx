23878fbdfba9b1d6f6cad7d43df56b6175b6dc67
Numberdar4141 <>
2024-04-13 12:59:57 +0530

    Update Features.tsx

--- src/components/homepage/Features.tsx
+++ src/components/homepage/Features.tsx
@@ -23,7 +23,7 @@ function Features() {
           {featuresCard.map(({ id, icon, name, desc }, idx) => (
             <div className="col-span-12  sm:col-span-6 lg:col-span-3 " key={id}>
               <StaggerEffect id={idx}>
-                <div className="bg-[#203A74]  p-6 xl:p-8 flex flex-col rounded-2xl shadow-xl  group hover:border-mainTextColor duration-700 hover:bg-white/75">
+                <div className="bg-[#203A74]  p-6 xl:p-8 flex flex-col lg:h-[430px] rounded-2xl shadow-xl  group hover:border-mainTextColor duration-700 hover:bg-white/75">
                   <div className="text-4xl text-s1 bg-white w-[53px] mb-5 rounded-full p-2 group-hover:text-mainTextColor duration-500">
                     {icon}
                   </div>
